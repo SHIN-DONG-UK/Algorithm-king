814fd17b3e8b3243a10381b7a33003c8133da948
ehd2610 <ehd2610@naver.com>
2024-08-25 14:20:42 +0900

    update

---
+++ BFS/[SWEA]탈주범 검거/bfs.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+struct Point
+{
+	int y;
+	int x;
+};
+
+// 상우하좌
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+
+int valid_dir[8][4] =
+{  //상우하좌
+	{0,0,0,0}, // 0
+	{1,1,1,1}, // 1
+	{1,0,1,0}, // 2
+	{0,1,0,1}, // 3
+	{1,1,0,0}, // 4
+	{0,1,1,0}, // 5
+	{0,0,1,1}, // 6
+	{1,0,0,1}  // 7
+};
+
+int N, M, R, C, L;
+int MAP[50][50];
+int visited[50][50];
+int ans;
+
+void init();
+void input();
+void process();
+void bfs(Point sp);
+void print();
+
+int main()
+{
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++)
+	{
+		init();
+		input();
+		process();
+
+		cout << "#" << tc << " " << ans << '\n';
+	}
+}
+
+void init()
+{
+	
+	ans = 0;
+}
+
+void input()
+{
+	cin >> N >> M >> R >> C >> L;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> MAP[i][j];
+		}
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			visited[i][j] = 1e9;
+		}
+	}
+}
+
+void process()
+{
+	visited[R][C] = 1;
+	bfs({ R,C });
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (visited[i][j] != 0 && visited[i][j] <= L) ans++;
+		}
+	}
+	//print();
+}
+void bfs(Point sp)
+{
+	queue<Point> q;
+	q.push(sp);
+	visited[sp.y][sp.x] = 1;
+
+	Point cp, np;
+	while (!q.empty())
+	{
+		cp = q.front();
+		q.pop();
+
+		for (int d = 0; d < 4; d++)
+		{
+			int type = MAP[cp.y][cp.x];
+			if (valid_dir[type][d] == 0) continue;
+
+			np = { cp.y + dy[d], cp.x + dx[d] };
+			if (np.y < 0 || np.y >= N || np.x < 0 || np.x >= M) continue;
+			type = MAP[np.y][np.x];
+			if (valid_dir[type][(d - 2 + 4) % 4] == 0) continue;
+			if (MAP[np.y][np.x] == 0) continue;
+			if (visited[np.y][np.x] <= visited[cp.y][cp.x] + 1) continue;
+			visited[np.y][np.x] = visited[cp.y][cp.x] + 1;
+			q.push(np);
+		}
+	}
+	
+}
+
+void print()
+{
+	cout << '\n';
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cout << visited[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
