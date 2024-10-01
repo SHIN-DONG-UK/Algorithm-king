7904e97cd8af7a5c67e956755f4e52aa47383fa3
신동욱 <140220959+SHIN-DONG-UK@users.noreply.github.com>
2024-10-01 17:28:16 +0900

    백트레킹 도장깨기 : N과M(1)

---
+++ 백트레킹/백준_15649_N과M(1)/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int N,M;
+int visited[9];
+int a[9];
+
+void input();
+void go(int idx);
+
+int main(){
+    input();
+    go(0);
+}
+
+void input(){
+    cin >> N >> M;
+}
+
+void go(int idx){
+    if(idx == M){
+        for(int i=0; i<M; i++){
+            cout << a[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=1; i<=N; i++){
+        if(visited[i] == 1) continue;
+        visited[i] = 1;
+        a[idx] = i;
+        go(idx + 1);
+        visited[i] = 0;
+    }
+}
