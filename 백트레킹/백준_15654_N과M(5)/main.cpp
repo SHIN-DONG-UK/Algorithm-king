54b5043c3a302de43b264e3169f79250331fd834
신동욱 <140220959+SHIN-DONG-UK@users.noreply.github.com>
2024-10-01 22:29:13 +0900

    백트레킹 도장깨기 : N과 M(5)

---
+++ 백트레킹/백준_15654_N과M(5)/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int a[8];
+int visited[8];
+int nums[8];
+
+void input();
+void go(int idx);
+
+int main(){
+    input();
+    go(0);
+}
+
+void input(){
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        cin >> nums[i];
+    }
+    sort(nums, nums+N);
+}
+
+void go(int idx){
+    if(idx == M){
+        for(int i=0; i<M; i++){
+            cout << nums[a[i]] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for(int i=0; i<N; i++){
+        if(visited[i] == 1) continue;
+        visited[i] = 1;
+        a[idx] = i;
+        go(idx + 1);
+        visited[i] = 0;
+    }
+}
