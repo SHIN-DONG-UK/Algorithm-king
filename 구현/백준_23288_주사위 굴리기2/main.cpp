5e4e1028e9104780a6d8a4d948cb42d0b7bfe54a
ehd2610 <ehd2610@naver.com>
2024-10-10 20:12:23 +0900

    백준 : 주사위 굴리기2(수정)

--- 구현/백준_23288_주사위 굴리기2/main.cpp
+++ 구현/백준_23288_주사위 굴리기2/main.cpp
@@ -14,15 +14,15 @@ struct Point {
 int dy[4] = { -1,0,1,0 };
 int dx[4] = { 0,1,0,-1 };
 
-int n, m;
+int n, m, k;
 int map[MAX_MAP][MAX_MAP];
 int visited[MAX_MAP][MAX_MAP];
-// 주사위 아랫면 == dice[2][1]
+// 주사위 아랫면 == dice[3][1]
 int dice[4][3] = {
-	{0,1,0},
+	{0,2,0},
+	{4,1,3},
 	{0,5,0},
-	{4,6,3},
-	{0,2,0}
+	{0,6,0}
 };
 
 int ans;
@@ -40,10 +40,10 @@ int main() {
 }
 
 void Input() {
-	cin >> n >> m;
+	cin >> n >> m >> k;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < m; j++)
 		{
 			cin >> map[i][j];
 		}
@@ -54,24 +54,22 @@ void Simulation() {
 	Point p = { 0,0 };
 	int dir = 1;
 
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < k; i++)
 	{
 		RollingDice(dir);
 		p = { p.y + dy[dir], p.x + dx[dir] };
 
 		// 1. 맵 밖으로 간 경우 : 반대방향으로 향하고 한 칸 전진
-		if (p.y < 0 || p.y >= n || p.x < 0 || p.x >= n) {
+		if (p.y < 0 || p.y >= n || p.x < 0 || p.x >= m) {
 			p = { p.y - (2 * dy[dir]), p.x - (2 * dx[dir]) };
 			dir = (dir + 2 + 4) % 4;
+			RollingDice(dir); 
 			RollingDice(dir); // 굴렸던거 원복
 		}
-		// 2. 아닌 경우 : dir 업데이트
-		else {
-			if (dice[2][1] > map[p.y][p.x]) dir = (dir + 4 + 1) % 4; // 시계 90도
-			else if (dice[2][1] < map[p.y][p.x]) dir = (dir + 4 - 1) % 4; // 반시계 90도
-			// 같으면 그냥 전진
-			
-		}
+
+		if (dice[3][1] > map[p.y][p.x]) dir = (dir + 4 + 1) % 4; // 시계 90도
+		else if (dice[3][1] < map[p.y][p.x]) dir = (dir + 4 - 1) % 4; // 반시계 90도
+		
 		ans += CalcScore(p);
 		
 	}
@@ -81,35 +79,37 @@ void RollingDice(int dir) {
 	int tmp;
 	// 상
 	if (dir == 0) {
-		tmp = dice[3][1];
-		dice[3][1] = dice[2][1];
-		dice[2][1] = dice[1][1];
-		dice[1][1] = dice[0][1];
-		dice[0][1] = tmp;
-	}
-	// 우
-	else if (dir == 1) {
-		tmp = dice[2][2];
-		dice[2][2] = dice[0][1];
-		dice[0][1] = dice[2][0];
-		dice[2][0] = dice[2][1];
-		dice[2][1] = tmp;
-	}
-	// 하
-	else if (dir == 2) {
 		tmp = dice[0][1];
 		dice[0][1] = dice[1][1];
 		dice[1][1] = dice[2][1];
 		dice[2][1] = dice[3][1];
 		dice[3][1] = tmp;
+		
+	}
+	// 우
+	else if (dir == 1) {
+		tmp = dice[3][1];
+		dice[3][1] = dice[1][2];
+		dice[1][2] = dice[1][1];
+		dice[1][1] = dice[1][0];
+		dice[1][0] = tmp;
+	}
+	// 하
+	else if (dir == 2) {
+		tmp = dice[3][1];
+		dice[3][1] = dice[2][1];
+		dice[2][1] = dice[1][1];
+		dice[1][1] = dice[0][1];
+		dice[0][1] = tmp;
+		
 	}
 	// 좌
 	else {
-		tmp = dice[2][0];
-		dice[2][0] = dice[0][1];
-		dice[0][1] = dice[2][2];
-		dice[2][2] = dice[2][1];
-		dice[2][1] = tmp;
+		tmp = dice[3][1];
+		dice[3][1] = dice[1][0];
+		dice[1][0] = dice[1][1];
+		dice[1][1] = dice[1][2];
+		dice[1][2] = tmp;
 	}
 }
 
@@ -130,7 +130,7 @@ int CalcScore(Point sp) {
 		for (int d = 0; d < 4; d++)
 		{
 			np = { cp.y + dy[d], cp.x + dx[d] };
-			if (np.y < 0 || np.y >= n || np.x < 0 || np.x >= n) continue;
+			if (np.y < 0 || np.y >= n || np.x < 0 || np.x >= m) continue;
 			if (visited[np.y][np.x] == 1) continue;
 			if (map[np.y][np.x] != map[sp.y][sp.x]) continue;
 
