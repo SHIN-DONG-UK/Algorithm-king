573634f1e78ce5f5e47769a6b9a6b77bfd12ed74
ehd2610 <ehd2610@naver.com>
2025-05-31 11:52:22 +0900

    modify: BOJ_24480_알고리즘 수업 - 깊이 우선 탐색2 README 수정

---
+++ Graph/BOJ_24480_알고리즘 수업 - 깊이 우선 탐색2/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M, R;
+vector<vector<int>> graph;
+int visited[100001];
+int order;
+
+void dfs(int now);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	cin >> N >> M >> R;
+	graph.resize(N + 1);
+	int from, to;
+	for (int i = 0; i < M; i++) {
+		cin >> from >> to;
+		graph[from].push_back(to);
+		graph[to].push_back(from);
+	}
+	for (int i = 1; i <= N; i++) {
+		sort(graph[i].begin(), graph[i].end(), greater<>());
+	}
+
+	visited[R] = 1;
+	order = 2;
+	dfs(R);
+
+	for (int i = 1; i <= N; i++) {
+		cout << visited[i] << '\n';
+	}
+	return 0;
+}
+
+void dfs(int now) {
+	for (int& next : graph[now]) {
+		if (visited[next] > 0) continue;
+		visited[next] = order++;
+		dfs(next);
+	}
+}
